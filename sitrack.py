7de0decf99cec55705460129e5b9d8b661c98c75
Andres Masanelli <masanelli.a@gmail.com>
2022-04-25 23:01:20 -0300

    update

--- sitrack.py
+++ sitrack.py
@@ -2,16 +2,21 @@ import argparse
 import logging
 import json
 import requests
+from datetime import datetime
 
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
 
+from google.cloud.bigquery import Client
+
 
 class GetApiData(beam.DoFn):
 
-    def __init__(self, auth):
+    def __init__(self, auth:str, year:int, month:int):
         self.auth = auth
+        self.year = year
+        self.month = month
         logging.debug('Fetching api data')
 
     def process(self, element):
@@ -21,9 +26,9 @@ class GetApiData(beam.DoFn):
         response = requests.post(
             api_url,
             json={
-                "processId": "43effed1-314b-4a8e-8e65-1115dc518c7d",
-                "year": 2022,
-                "month": [1, 2, 3, 4]
+                'processId': '43effed1-314b-4a8e-8e65-1115dc518c7d',
+                'year': self.year,
+                'month': [self.month]
             },
             headers={
                 'Authorization': self.auth
@@ -34,7 +39,28 @@ class GetApiData(beam.DoFn):
         return json_response['recipeHistory']
 
 
+def delete_records(table: str, year: int, month: int):
+
+    client = Client()
+
+    sql = """
+    DELETE 
+    FROM 
+        {}
+    WHERE
+        Year = {} AND
+        Month = {}
+    """.format(table, year, month)
+
+    job = client.query(sql)
+    job.result()
+
+    logging.debug('Records deleted')
+
+
 def run(argv=None, save_main_session=True):
+
+    today = datetime.today()
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -43,9 +69,46 @@ def run(argv=None, save_main_session=True):
         required=True,
         help='Sitrack API key'
     )
+    parser.add_argument(
+        '--project',
+        dest='project',
+        required=True,
+        help='GCP project'
+    )
+    parser.add_argument(
+        '--dataset',
+        dest='dataset',
+        required=True,
+        help='BigQuery dataset'
+    )
+    parser.add_argument(
+        '--table',
+        dest='table',
+        required=True,
+        help='BigQuery table'
+    )
+    parser.add_argument(
+        '--year',
+        dest='year',
+        default=today.year,
+        type=int,
+        help='Year to query'
+    )
+    parser.add_argument(
+        '--month',
+        dest='month',
+        default=today.month,
+        type=int,
+        help='Month to query'
+    )
     known_args, pipeline_args = parser.parse_known_args(argv)
 
-    pipeline_options = PipelineOptions(pipeline_args)
+    delete_records('{}.{}.{}'.format(known_args.project, known_args.dataset, known_args.table), known_args.year, known_args.month)
+
+    pipeline_options = PipelineOptions(
+        pipeline_args, 
+        project=known_args.project
+    )
     pipeline_options.view_as(
         SetupOptions
     ).save_main_session = save_main_session
@@ -55,21 +118,21 @@ def run(argv=None, save_main_session=True):
         lines = (
             p
             | 'Create' >> beam.Create(['Start'])
-            | 'Fetch API Data' >> beam.ParDo(GetApiData(auth=known_args.auth))
+            | 'Fetch API Data' >> beam.ParDo(GetApiData(auth=known_args.auth, year=known_args.year, month=known_args.month))
         )
 
         counts = (
             lines
             | 'Count Fields' >> beam.Map(lambda x: (x['landName'], len(x['lotName'].split(', '))))
             | 'Group and Sum' >> beam.CombinePerKey(sum)
-            | 'Convert to Dict' >> beam.Map(lambda x: dict(Campo=x[0], Recetas=x[1]))
+            | 'Convert to Dict' >> beam.Map(lambda x: dict(Year=known_args.year, Month=known_args.month, Campo=x[0], Recetas=x[1]))
             #| beam.Map(print)
         )
 
         counts | 'Write to BigQuery' >> beam.io.WriteToBigQuery(
-            'green-eye-cloud:warehouse.test',
-            schema='Campo:STRING,Recetas:INTEGER',
-            write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
+            '{}:{}.{}'.format(known_args.project, known_args.dataset, known_args.table),
+            schema='Year:INTEGER,Month:INTEGER,Campo:STRING,Recetas:INTEGER',
+            write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
             create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED
         )
 
