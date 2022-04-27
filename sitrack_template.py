e7a5baa52372879c9dd979f97709a345ad8a3277
Andres Masanelli <masanelli.a@gmail.com>
2022-04-27 00:30:34 -0300

    update

--- sitrack_template.py
+++ sitrack_template.py
@@ -7,7 +7,7 @@ from apache_beam.options.pipeline_options import PipelineOptions
 
 class GetApiData(beam.DoFn):
 
-    def __init__(self, auth: str, year: int, month: int):
+    def __init__(self, auth: str, year: str, month: str):
         self.auth = auth
         self.year = year
         self.month = month
@@ -21,8 +21,8 @@ class GetApiData(beam.DoFn):
             api_url,
             json={
                 'processId': '43effed1-314b-4a8e-8e65-1115dc518c7d',
-                'year': self.year,
-                'month': [self.month]
+                'year': int(self.year),
+                'month': [int(self.month)]
             },
             headers={
                 'Authorization': self.auth
@@ -58,18 +58,18 @@ class TemplateOptions(PipelineOptions):
             type=str,
             help='BigQuery table'
         )
-        parser.add_argument(
+        parser.add_value_provider_argument(
             '--year',
             dest='year',
-            required=True,
-            type=int,
+            default='2022',
+            type=str,
             help='Year to query'
         )
-        parser.add_argument(
+        parser.add_value_provider_argument(
             '--month',
             dest='month',
-            required=True,
-            type=int,
+            default='3',
+            type=str,
             help='Month to query'
         )
 
@@ -77,30 +77,29 @@ class TemplateOptions(PipelineOptions):
 def run():
 
     pipeline_options = PipelineOptions(flags=None, save_main_session=True)
-    pipeline_options.view_as(TemplateOptions)
-
-    options = pipeline_options.get_all_options()
+    
+    options = pipeline_options.view_as(TemplateOptions)
 
     with beam.Pipeline(options=pipeline_options) as p:
 
         lines = (
             p
             | 'Create' >> beam.Create(['Start'])
-            | 'Fetch API Data' >> beam.ParDo(GetApiData(auth=options['auth'], year=options['year'], month=options['month']))
+            | 'Fetch API Data' >> beam.ParDo(GetApiData(auth=options.auth, year=options.year, month=options.month))
         )
 
         counts = (
             lines
             | 'Count Fields' >> beam.Map(lambda x: (x['landName'], len(x['lotName'].split(', '))))
             | 'Group and Sum' >> beam.CombinePerKey(sum)
-            | 'Convert to Dict' >> beam.Map(lambda x: dict(Year=options['year'], Month=options['month'], Campo=x[0], Recetas=x[1]))
+            | 'Convert to Dict' >> beam.Map(lambda x: dict(Year=options.year, Month=options.month, Campo=x[0], Recetas=x[1]))
             # | beam.Map(print)
         )
 
         counts | 'Write to BigQuery' >> beam.io.WriteToBigQuery(
-            '{}:{}.{}'.format(options['project'],
-                              options['dataset'],
-                              options['table']),
+            '{}:{}.{}'.format(pipeline_options.get_all_options()['project'],
+                              options.dataset,
+                              options.table),
             schema='Year:INTEGER,Month:INTEGER,Campo:STRING,Recetas:INTEGER',
             write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
             create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED
